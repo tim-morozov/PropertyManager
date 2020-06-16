0357487b99380ec2591c52c97dfb89efaa34bf4b
tim-morozov <tim.morozov@gmail.com>
2020-06-16 09:33:23 -0500

    added Contractor Model

---
+++ PropertyManager/Models/Contractor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyManager.Models
+{
+    public class Contractor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string Type { get; set; }
+    }
+}
